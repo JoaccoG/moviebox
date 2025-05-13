33501d3ab44beda5e90f2a757d209242369f958f
Joacco <joaquingodoy2407@gmail.com>
2025-05-13 13:41:45 -0300

    feat: add moviedetails component

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => {
     <header className="flex items-center p-s">
       <Link to="/" className="flex items-center">
         <img src="/assets/img/logo.svg" alt="MovieBox Logo" className="w-[35px] h-[35px] md:w-[50px] md:h-[50px]" />
-        <h1 className="ml-xs md:ml-s color-primary font-weight-bold text-s md:text-xl">MovieBox</h1>
+        <h1 className="ml-xs md:ml-s color-neutral-light font-weight-bold text-s md:text-xl header-title">MovieBox</h1>
       </Link>
     </header>
   );
